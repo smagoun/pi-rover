ecc7485b52fd3e1626996da9a7303e517948dc40
Steve Magoun <smagoun@gmail.com>
2020-11-22 18:01:31 -0500

    Cleanup + doc

--- rover-client.js
+++ rover-client.js
@@ -3,16 +3,19 @@
 
 let socket = null;
 
-// Identifier provided by the server
-let client_id = 0;
-
 // Counter for messages we send
 let message_id = 0;
 
 // Message timeout, in ms
 const MSG_TIMEOUT = 3000;
 
-function connect(host, port, openCallback, closeCallback) {
+/**
+ * Open a connection to the WebSocker server
+ *
+ * @param {String} host Hostname to connect to
+ * @param {number} port Port to connect to
+ */
+function connect(host, port) {
     if (socket != null) {
 	if (socket.readyState != WebSocket.CLOSED) {
 	    socket.close();
@@ -29,35 +32,47 @@ function connect(host, port, openCallback, closeCallback) {
     socket.onerror = errorCallback;
 }
 
-// Send a command to the server
-//
-// Requires that the socket is already open
+/**
+ * Send a command to the server. Requires that the socket is already open.
+ *
+ * @param {String} cmd One of CMD_*
+ */
 function sendCommand(cmd) {
+    if (socket == null) {
+	console.log(`Unable to send message, socket is null`);
+	return;
+    }
+    let state = socket.readyState;
+    if (socket.readyState !== WebSocket.OPEN) {
+	console.log(`Unable to send message, socket not ready (state is ${state})`);
+	return;
+    }
     let msg = {
-	client_id: client_id,
 	message_id: message_id,
 	command: cmd,
 	date: Date.now(),
     };
-    if (socket != null) {
-	let state = socket.readyState;
-	if (state === WebSocket.OPEN) {
-	    console.log(`sending command: ${JSON.stringify(msg)}`);
-	    socket.send(JSON.stringify(msg));
-	    message_id++;
-	} else {
-	    console.log(`Unable to send message, socket not ready (state is ${state})`);
-	}
-    } else {
-	console.log(`Unable to send message, socket is null`);
-    }
+    console.log(`sending command: ${JSON.stringify(msg)}`);
+    socket.send(JSON.stringify(msg));
+    message_id++;
 }
 
+/**
+ * Handle a button press from the web page
+ *
+ * @param {String} cmd One of the CMD_* commands
+ * @param {element} element DOM element of the button that was clicked
+ */
 function buttonPress(cmd, element) {
     console.log(`${cmd} button pressed`);
     sendCommand(cmd);
 }
 
+/**
+ * Callback function for socket open events
+ *
+ * @param {Event} evt 
+ */
 function openCallback(evt) {
     console.log('socket opened', evt);
     disableElement('btn_connect');
@@ -65,6 +80,11 @@ function openCallback(evt) {
     sendCommand(protocol.CMD_REQUEST_CONTROL);
 }
 
+/**
+ * Callback function for socket close events. Reverts the UI to its original state
+ *
+ * @param {Event} evt 
+ */
 function closeCallback(evt) {
     console.log('socket closed', evt);
     socket = null;
@@ -79,6 +99,11 @@ function closeCallback(evt) {
     }
 }
 
+/**
+ * Callback function for socket errors
+ *
+ * @param {Event} evt 
+ */
 function errorCallback(evt) {
     console.log('socket error', evt);
     socket = null;
@@ -91,6 +116,11 @@ function errorCallback(evt) {
     writeStatus('Error communicating with rover', MSG_TIMEOUT);
 }
 
+/**
+ * Callback function for messages received from the server
+ *
+ * @param {Event} evt 
+ */
 function messageCallback(evt) {
     console.log('message from server', evt);
     if (evt.type === 'message') {
@@ -105,6 +135,12 @@ function messageCallback(evt) {
     }
 }
 
+/**
+ * Write a status message to the UI
+ *
+ * @param {String} msg Message to be written
+ * @param {number} timeout Length of time to display the message, in ms
+ */
 async function writeStatus(msg, timeout) {
     msgs = document.getElementById('msgs');
     msgs.innerText = msg;
@@ -127,10 +163,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Open a connection to the server and request control of the rover
+ *
+ * @param {String} host Hostname to connect to
+ * @param {number} port Port to connect to
+ */
 function requestControl(host, port) {
-    connect(host, port, openCallback, closeCallback);
+    connect(host, port);
 }
 
+/**
+ * Tell the server we no longer want to control the rover
+ */
 function cedeControl() {
     if (socket != null) {
 	sendCommand(protocol.CMD_CEDE_CONTROL);
@@ -138,9 +183,20 @@ function cedeControl() {
     }
 }
 
+/**
+ * Mark an element as enabled
+ *
+ * @param {String} id ID of a DOM element
+ */
 function enableElement(id) {
     document.getElementById(id).disabled = false;
 }
+
+/**
+ * Mark an element as disabled
+ *
+ * @param {String} id ID of a DOM element
+ */
 function disableElement(id) {
     document.getElementById(id).disabled = true;
 }
